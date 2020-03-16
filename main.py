af4106b540b41f3023faf65969262e32b8e0ae28
KrashR13 <KrashR26@gmail.com>
2020-03-16 17:01:04 +0300

    Изменение кода в функцию

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
-vk_session=vk_api.VkApi(token='fcb715bac83de58c82745490f78b4bc7264bf23035dbff8d35c1531193f8acc43113122ad9866cb78e6a6') #Сообщество https://vk.com/club192951175
-longpoll=VkBotLongPoll(vk_session,'192951175')
 
-for event in longpoll.listen():
 
-    if event.type == VkBotEventType.MESSAGE_NEW:
-        print('Новое сообщение:')
 
-        print('Для меня от: ', end='')
+def vkbot():
+    vk_session=vk_api.VkApi(token='fcb715bac83de58c82745490f78b4bc7264bf23035dbff8d35c1531193f8acc43113122ad9866cb78e6a6') #Сообщество https://vk.com/club192951175
+    longpoll=VkBotLongPoll(vk_session,'192951175')
 
-        print(event.obj.message["peer_id"])
+    for event in longpoll.listen():
 
-        print('Текст:', event.obj.message["text"])
-        print()
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            print('Новое сообщение:')
+
+            print('Для меня от: ', end='')
+
+            print(event.obj.message["peer_id"])
+
+            print('Текст:', event.obj.message["text"])
+            print()
+
+
+
+
 
